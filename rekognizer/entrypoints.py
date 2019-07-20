1e01b38a5090f6593ab368434e9fac6a9145aef8
Walkoss <Walkoss@users.noreply.github.com>
2019-07-21 00:51:30 +0200

    Added CORS

--- rekognizer/entrypoints.py
+++ rekognizer/entrypoints.py
@@ -1,8 +1,8 @@
 import json
+import types
 
 from marshmallow import ValidationError
 from nameko.exceptions import safe_for_serialization, BadRequest
-from nameko.web.handlers import HttpRequestHandler
 from werkzeug import Response
 
 from rekognizer.exceptions import (
@@ -11,8 +11,12 @@ from rekognizer.exceptions import (
     UnknownPersonException,
 )
 
+from functools import partial
+from nameko.web.handlers import HttpRequestHandler
+from nameko.extensions import register_entrypoint
 
-class HttpEntrypoint(HttpRequestHandler):
+
+class CorsHttpRequestHandler(HttpRequestHandler):
     """ Overrides `response_from_exception` so we can customize error handling.
     """
 
@@ -40,5 +44,58 @@ class HttpEntrypoint(HttpRequestHandler):
             mimetype="application/json",
         )
 
+    """
+    A Cors Http handler. 
+    Registers an OPTIONS route per endpoint definition.
+    """
+    def __init__(self, method, url, expected_exceptions=(), **kwargs):
+        super().__init__(method, url, expected_exceptions=expected_exceptions)
+        self.allowed_origin = kwargs.get('origin', ['*'])
+        self.allowed_methods = kwargs.get('methods', ['*'])
 
-http = HttpEntrypoint.decorator
+    def handle_request(self, request):
+        self.request = request
+        if request.method == 'OPTIONS':
+            return self.response_from_result(result='')
+        return super().handle_request(request)
+
+    def response_from_result(self, *args, **kwargs):
+        response = super(CorsHttpRequestHandler, self).response_from_result(*args, **kwargs)
+        response.headers.add("Access-Control-Allow-Headers",
+                             self.request.headers.get("Access-Control-Request-Headers"))
+        response.headers.add("Access-Control-Allow-Methods", ",".join(self.allowed_methods))
+        response.headers.add("Access-Control-Allow-Origin", ",".join(self.allowed_origin))
+        return response
+
+    @classmethod
+    def decorator(cls, *args, **kwargs):
+        """
+        We're overriding the decorator classmethod to allow it to register an options
+        route for each standard REST call. This saves us from manually defining OPTIONS
+        routes for each CORs enabled endpoint
+        """
+        def registering_decorator(fn, args, kwargs):
+            instance = cls(*args, **kwargs)
+            register_entrypoint(fn, instance)
+            if instance.method in ('GET', 'POST', 'DELETE', 'PUT') and \
+                    ('*' in instance.allowed_methods or instance.method in instance.allowed_methods):
+                options_args = ['OPTIONS'] + list(args[1:])
+                options_instance = cls(*options_args, **kwargs)
+                register_entrypoint(fn, options_instance)
+            return fn
+
+        if len(args) == 1 and isinstance(args[0], types.FunctionType):
+            # usage without arguments to the decorator:
+            # @foobar
+            # def spam():
+            #     pass
+            return registering_decorator(args[0], args=(), kwargs={})
+        else:
+            # usage with arguments to the decorator:
+            # @foobar('shrub', ...)
+            # def spam():
+            #     pass
+            return partial(registering_decorator, args=args, kwargs=kwargs)
+
+
+http = CorsHttpRequestHandler.decorator
